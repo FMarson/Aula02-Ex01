d67ed3d33316a51e4bf8331d5c880040f50602c8
Fernando Marson <fernando.marson@gmail.com>
2024-09-09 20:49:03 +0100

    first commit

---
+++ Mago.java
@@ -0,0 +1,20 @@
+/*
+ * Crie uma classe Mago em Java que modele um personagem de um jogo de RPG. A classe deve possuir os seguintes atributos:
+
+nome (String): O nome do mago.
+tipoDeMagia (String): O tipo de magia que o mago utiliza (ex: fogo, gelo, etc.).
+pontosDeVida (int): Os pontos de vida do mago.
+pontosDeMana (int): Os pontos de mana do mago.
+Além dos métodos getters e setters para todos os atributos, a classe deve conter os seguintes métodos:
+
+lancarMagia: Este método deve gastar uma quantidade específica de pontos de mana e exibir uma mensagem indicando que a magia foi lançada. Se o mago não tiver mana suficiente, uma mensagem indicando isso deve ser exibida.
+
+lancarMagiaEspecial: Este método deve gastar uma quantidade maior de pontos de mana do que o método anterior e exibir uma mensagem indicando que uma magia especial foi lançada. Caso o mago não tenha mana suficiente, uma mensagem apropriada deve ser exibida.
+
+curar: Este método deve receber a quantidade de pontos de vida a serem recuperados como parâmetro e aumentar os pontos de vida do mago em acordo com esse valor. A quantidade de pontos de vida não pode ultrapassar um valor máximo (defina um valor máximo para pontos de vida).
+
+recuperarMana: Este método deve receber a quantidade de pontos de mana a serem recuperados como parâmetro e aumentar os pontos de mana do mago em acordo com esse valor. A quantidade de pontos de mana não pode ultrapassar um valor máximo (defina um valor máximo para pontos de mana).
+ * */
+public class Mago {
+    
+}
